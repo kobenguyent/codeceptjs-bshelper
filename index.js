9597f88bffa62882c9aa96dcc255b6e2c8ddb695
Thanh Nguyen <peter.nguyentr@gmail.com>
2019-03-22 13:49:37 +0100

    initialize the bs helper

---
+++ index.js
@@ -0,0 +1,88 @@
+/**
+ * Browserstack Helper for Codeceptjs
+ *
+ * @author PeterNgTr
+ */
+class BrowserstackHelper extends Helper {
+
+    constructor(config) {
+        super(config);
+    }
+
+    /**
+     *
+     * @param sessionId Session ID for current Test browser session
+     * @param data      Test name, etc
+     * @private
+     */
+    _updateBuild(sessionId, data) {
+        this.helpers['REST']._executeRequest({
+            url: `https://api.browserstack.com/automate/sessions/${sessionId}.json`,
+            method: 'put',
+            data: data,
+            auth: {
+                'username': this.config.user,
+                'password': this.config.key
+            }
+        });
+
+        this._exposeBuildLink(sessionId);
+    }
+
+    /**
+     *
+     * @param sessionId Session ID for current Test browser session
+     * @private
+     */
+    _exposeBuildLink(sessionId) {
+        this.helpers['REST']._executeRequest({
+            url: `https://api.browserstack.com/automate/sessions/${sessionId}.json`,
+            method: 'get',
+            auth: {
+                'username': this.config.user,
+                'password': this.config.key
+            }
+        }).then(res => {
+            const bs_url = `Test finished. Link to job: ${res.data.automation_session.public_url}`;
+            console.log(bs_url);
+        });
+    }
+
+    /**
+     * Helper function gets called if the test is passing
+     * @param test
+     * @private
+     */
+    _passed(test) {
+        console.log("Test has Passed");
+        const sessionId = this._getSessionId();
+        this._updateBuild(sessionId, { 'status': 'passed', 'name': test.title });
+    }
+
+    /**
+     * Helper function gets called if the test execution fails
+     * @param test
+     * @param error
+     * @private
+     */
+    _failed(test, error) {
+        console.log("Test has failed");
+        const sessionId = this._getSessionId();
+        this._updateBuild(sessionId, { 'status': 'failed', 'name': test.title });
+    }
+
+    _getSessionId() {
+        if (this.helpers['WebDriver']) {
+            return this.helpers['WebDriver'].browser.sessionId;
+        }
+        if (this.helpers['Appium']) {
+            return this.helpers['Appium'].browser.sessionId;
+        }
+        if (this.helpers['WebDriverIO']) {
+            return this.helpers['WebDriverIO'].browser.requestHandler.sessionID;
+        }
+        throw new Error('No matching helper found. Supported helpers: WebDriver/Appium/WebDriverIO');
+    }
+}
+
+module.exports = BrowserstackHelper;
---
+++ readme.md
@@ -0,0 +1,27 @@
+# codeceptjs-bshelper
+CodeceptJS Browserstack helper, to update Test Names, Test Results after execution 
+
+codeceptjs-bshelper is [CodeceptJS](https://codecept.io/) helper which is to complete tests results on Browserstack after execution. The helper allows to update test name and
+test results on Browserstack using the `_passed` and `_failed` hooks accessible in the helper.
+
+NPM package: https://www.npmjs.com/package/codeceptjs-bshelper
+
+### Configuration
+
+This helper should be added in codecept.json/codecept.conf.js
+
+Example:
+
+```json
+{
+   "helpers": {
+     "BrowserstackHelper": {
+      "require": "codeceptjs-bshelper",
+      "user": "BROWSERSTACK_USERNAME",
+      "key": "BROWSERSTACK_ACCESS_KEY"
+    },
+    "REST: {}
+   }
+}
+```
+To use this helper, users must provide the Browserstack User, Key & Host as part of the configuration.
