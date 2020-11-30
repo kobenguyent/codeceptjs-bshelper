9b283cd707d5003c25efeabfd86b6e5485248d44
PeterNgTr <thanh.nguyen@tamara.co>
2020-11-30 14:37:28 +0100

    update bshelper as plugin

--- index.js
+++ index.js
@@ -1,98 +1,126 @@
 const tinyurl = require('tinyurl');
+
 const axios = require('axios');
 
+const { event } = require('codeceptjs');
+
+const Container = require('codeceptjs').container;
+
+const helpers = Container.helpers();
+
+const output = require('./lib/output');
+
+let helper;
+
+const supportedHelpers = [
+  'WebDriver',
+  'Appium',
+];
+
+for (const helperName of supportedHelpers) {
+  if (Object.keys(helpers).indexOf(helperName) > -1) {
+    helper = helpers[helperName];
+  }
+}
+
+const defaultConfig = {
+  user: '',
+  key: '',
+  shortUrl: false,
+  enabled: false,
+};
+
 /**
- * Browserstack Helper for Codeceptjs
+ * Browserstack plugin for Codeceptjs
  *
  * @author PeterNgTr
  */
-class BrowserstackHelper extends Helper {
-  /**
-     *
-     * @param sessionId Session ID for current Test browser session
-     * @param data      Test name, etc
-     * @private
-     */
-  async _updateBuild(sessionId, data) {
-    if ((this.config.user && this.config.key) && (this.config.user !== '' && this.config.key !== '')) {
-      await axios({
-        url: `https://api.browserstack.com/automate/sessions/${sessionId}.json`,
-        method: 'put',
-        data,
-        auth: {
-          username: this.config.user,
-          password: this.config.key,
-        },
-      });
-
-      await this._exposeBuildLink(sessionId);
-    } else {
-      console.log('There is no provided Browserstack credentials. Probably you are not running with Browserstack!');
-    }
-  }
+module.exports = (config) => {
+  const currentConfig = Object.assign(defaultConfig, config);
+  if (currentConfig.user === '' || currentConfig.password === '') throw new Error('Please provide proper Browserstack credentials');
 
   /**
-     *
-     * @param sessionId Session ID for current Test browser session
-     * @private
-     */
-  async _exposeBuildLink(sessionId) {
+   *
+   * @param {string} url
+   * @private
+   *
+   */
+  this._shortenUrl = async function (url) {
+    const shortenUrl = await tinyurl.shorten(url);
+    return shortenUrl;
+  };
+
+  /**
+   *
+   * @param {string} sessionId Session ID for current Test browser session
+   * @private
+   *
+   */
+  this._exposeBuildLink = async function (sessionId) {
     const res = await axios({
       url: `https://api.browserstack.com/automate/sessions/${sessionId}.json`,
       method: 'get',
       auth: {
-        username: this.config.user,
-        password: this.config.key,
+        username: currentConfig.user,
+        password: currentConfig.key,
       },
     });
 
     let exposedUrl;
 
-    if (this.config.shortUrl) {
+    if (currentConfig.shortUrl) {
       exposedUrl = await this._shortenUrl(res.data.automation_session.public_url);
     } else {
       exposedUrl = res.data.automation_session.public_url;
     }
 
-    console.log(`Link to job:\n${exposedUrl}\n`);
+    output.log(`Link to job:\n${exposedUrl}\n`);
     return exposedUrl;
-  }
+  };
 
   /**
-     * Helper function gets called if the test is passing
-     * @param test
-     * @private
-     */
-  async _passed(test) {
+   *
+   * @param {string} sessionId Session ID for current Test browser session
+   * @param {object} data Test name, etc
+   * @private
+   */
+  this._updateBuild = async function(sessionId, data) {
+    if ((currentConfig.user && currentConfig.key) && (currentConfig.user !== '' && currentConfig.key !== '')) {
+      await axios({
+        url: `https://api.browserstack.com/automate/sessions/${sessionId}.json`,
+        method: 'put',
+        data,
+        auth: {
+          username: currentConfig.user,
+          password: currentConfig.key,
+        },
+      });
+
+      await this._exposeBuildLink(sessionId);
+    } else {
+      output.log('There is no provided Browserstack credentials. Probably you are not running with Browserstack!');
+    }
+  };
+
+  event.dispatcher.on(event.test.passed, async (test) => {
     const sessionId = this._getSessionId();
     await this._updateBuild(sessionId, { status: 'passed', name: test.title });
-  }
+  });
 
-  /**
-     * Helper function gets called if the test execution fails
-     * @param test
-     * @param error
-     * @private
-     */
-  async _failed(test) {
+  event.dispatcher.on(event.test.failed, async (test, err) => {
     const sessionId = this._getSessionId();
-    await this._updateBuild(sessionId, { status: 'failed', name: test.title, reason: test.err.message });
-  }
+    await this._updateBuild(sessionId, { status: 'failed', name: test.title, reason: err });
+  });
 
-  _getSessionId() {
-    if (this.helpers.WebDriver) {
-      return this.helpers.WebDriver.browser.sessionId;
+  this._getSessionId = function () {
+    if (helper.WebDriver) {
+      return helper.WebDriver.browser.sessionId;
     }
-    if (this.helpers.Appium) {
-      return this.helpers.Appium.browser.sessionId;
+    if (helper.Appium) {
+      return helper.Appium.browser.sessionId;
     }
     throw new Error('No matching helper found. Supported helpers: WebDriver/Appium');
-  }
+  };
 
-  async _shortenUrl(url) {
-    const shortenUrl = await tinyurl.shorten(url);
-    return shortenUrl;
-  }
-}
-
-module.exports = BrowserstackHelper;
+  return this;
+};
--- test/index_test.js
+++ test/index_test.js
@@ -2,38 +2,71 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const axios = require('axios');
 const MockAdapter = require('axios-mock-adapter');
+const bsPlugin = require('../index.js');
 
 const mock = new MockAdapter(axios);
 
-// Required to avoid class undefined errors when initializing our helper
-global.Helper = require('codeceptjs').helper;
+describe('Browserstack plugin config', () => {
+  it('no credentials provided ', () => {
+    try {
+      bsPlugin();
+    } catch (e) {
+      expect(e.message).contain('Please provide proper Browserstack credentials');
+    }
+  });
 
-const BrowserstackHelper = require('../index.js');
+  it('empty username ', () => {
+    try {
+      bsPlugin({
+        user: '',
+        key: 'test',
+        shortUrl: false,
+      });
+    } catch (e) {
+      expect(e.message).contain('Please provide proper Browserstack credentials');
+    }
+  });
+
+  it('empty password ', () => {
+    try {
+      bsPlugin({
+        user: 'test',
+        key: '',
+        shortUrl: false,
+      });
+    } catch (e) {
+      expect(e.message).contain('Please provide proper Browserstack credentials');
+    }
+  });
+});
 
 describe('#_getSessionId', () => {
   let stub;
-  let bsHelper;
-
+  let bs;
   beforeEach(() => {
-    bsHelper = new BrowserstackHelper();
+    bs = bsPlugin({
+      user: 'test',
+      key: 'test',
+      shortUrl: false,
+    });
   });
 
   it('should get sessionId of wd', () => {
-    stub = sinon.stub(bsHelper, '_getSessionId').callsFake(() => 'WebDriver');
-    expect(bsHelper._getSessionId()).to.be.equal('WebDriver');
+    stub = sinon.stub(bs, '_getSessionId').callsFake(() => 'WebDriver');
+    expect(bs._getSessionId()).to.be.equal('WebDriver');
     stub.restore();
   });
 
   it('should get sessionId of Appium', () => {
-    stub = sinon.stub(bsHelper, '_getSessionId').callsFake(() => 'Appium');
-    expect(bsHelper._getSessionId()).to.be.equal('Appium');
+    stub = sinon.stub(bs, '_getSessionId').callsFake(() => 'Appium');
+    expect(bs._getSessionId()).to.be.equal('Appium');
     stub.restore();
   });
 
   it('should throw error', () => {
-    stub = sinon.stub(bsHelper, '_getSessionId').callsFake(() => '');
+    stub = sinon.stub(bs, '_getSessionId').callsFake(() => '');
     try {
-      bsHelper._getSessionId();
+      bs._getSessionId();
     } catch (error) {
       expect(error.message).to.equal('No matching helper found. Supported helpers: WebDriver/Appium');
     }
@@ -41,42 +74,52 @@ describe('#_getSessionId', () => {
 });
 
 describe('#_shortenUrl', () => {
-  let bsHelper;
-
-  beforeEach(() => {
-    bsHelper = new BrowserstackHelper();
+  const bs = bsPlugin({
+    user: 'test',
+    key: 'test',
+    shortUrl: false,
   });
 
   it('should return short url', async () => {
-    const res = await bsHelper._shortenUrl('https://thisisasuperlongtext.domain');
-    expect(res).to.contain('http://tinyurl.com/');
+    const res = await bs._shortenUrl('https://thisisasuperlongtext.domain');
+    expect(res).to.contain('https://tinyurl.com/');
   });
 });
 
-describe('#_exposeBuildLink', () => {
-  let bsHelper;
-  const sessionId = '1234';
-  mock.onGet(`https://api.browserstack.com/automate/sessions/${sessionId}.json`).reply(200, { automation_session: { public_url: 'http://test.link' } });
+describe('#_exposeBuildLink - with shortUrl', () => {
+  const sessionId = '4567';
+  mock.onGet(`https://api.browserstack.com/automate/sessions/${sessionId}.json`).reply(200, { automation_session: { public_url: 'http://test.link.abc' } });
+  let bs;
 
-  describe('without shortUrl', () => {
-    beforeEach(() => {
-      bsHelper = new BrowserstackHelper({ user: 'test', key: 'test' });
-    });
-
-    it('should return the build link', async () => {
-      const res = await bsHelper._exposeBuildLink(sessionId);
-      expect(res).to.be.equal('http://test.link');
+  beforeEach(() => {
+    bs = bsPlugin({
+      user: 'test',
+      key: 'test',
+      shortUrl: true,
     });
   });
 
-  describe('with shortUrl', () => {
-    beforeEach(() => {
-      bsHelper = new BrowserstackHelper({ user: 'test', key: 'test', shortUrl: true });
-    });
+  it('should return the build link', async () => {
+    const res = await bs._exposeBuildLink(sessionId);
+    expect(res).to.contain('https://tinyurl.com/');
+  });
+});
 
-    it('should return the build link', async () => {
-      const res = await bsHelper._exposeBuildLink(sessionId);
-      expect(res).to.contain('http://tinyurl.com/');
+describe('#_exposeBuildLink - without shortUrl', () => {
+  const sessionId = '4567';
+  mock.onGet(`https://api.browserstack.com/automate/sessions/${sessionId}.json`).reply(200, { automation_session: { public_url: 'http://test.link' } });
+  let bs;
+
+  beforeEach(() => {
+    bs = bsPlugin({
+      user: 'test',
+      key: 'test',
+      shortUrl: false,
     });
+  });
+
+  it('should return the build link', async () => {
+    const res = await bs._exposeBuildLink(sessionId);
+    expect(res).to.be.equal('http://test.link');
   });
 });
